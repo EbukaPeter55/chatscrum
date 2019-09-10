ba713af1bbe00bb9756896e4cd42ffa09d1276ad
jeffersoniyobosa <iyobosajefferson2@gmail.com>
2019-09-10 17:38:07 +0100

    Made numerous modifications

--- chatscrum/src/app/teamtaskcards/teamtaskcards.component.ts
+++ chatscrum/src/app/teamtaskcards/teamtaskcards.component.ts
@@ -12,67 +12,49 @@ export class TeamtaskcardsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    //Jquery here
-//     // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image that opens the modal
-var imgg = document.getElementById("myimg");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the image, open the modal 
-imgg.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
- function trigger(){
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  
-  if (event.target == modal) {
-    modal.style.display = "none";
-    console.log(event.target)
-  }
-}
-
-//script for edit task modal
-// Get the modal
-// var edit = document.getElementById("myEdit");
-
-// // Get the image that opens the modal
-// var mag = document.getElementById("myimg-2");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("clos")[0];
-
-// // When the user clicks the image, open the modal 
-// mag.onclick = function() {
-//   edit.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-//  function pop(){
-//   edit.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-  
-//   if (event.target == edit) {
-//     console.log(event.target);
-//     edit.style.display = "none";
     
-//   }
-// }
+    // Initialise Modal  function
+    window.addEventListener("load", toggleModal)
+      function toggleModal() {
+        let modalTrigger = document.querySelectorAll(".modal_wrapper");
+        let overlay = document.querySelector(".overlay");
+
+        // modalTrigger.forEach(trigger => {
+          for (let i = 0; i < modalTrigger.length; i++) {
+            const trigger = modalTrigger[i];
+            // Get the modal element under the index
+            let modal = trigger.querySelector(".modal");
+            let close = modal.querySelector(".cancel");
+            // Functions to call to hide or show
+              let showModal = ()=>{
+                modal.classList.remove("hidden");
+                overlay.classList.remove("hidden");
+              };
+              let hideModal = ()=>{
+                modal.classList.add("hidden");
+                overlay.classList.add("hidden");
+              };
+            // Call the function
+            trigger.addEventListener('click', ()=>{
+              showModal();
+            });
+            close.addEventListener("click", ()=>{
+              hideModal();
+            });
+
+          // Close the modal if user clicks anywhere outside the modal
+          window.onclick = (event)=>{
+            if (event.target === overlay) {
+              hideModal()
+            }
+          }
+        };
+
+
+        
+        
+      }
+      
+    
   }
-
-
-  
-
 }
