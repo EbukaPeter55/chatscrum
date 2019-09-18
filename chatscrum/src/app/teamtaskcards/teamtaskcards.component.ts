25a1906130bcea71f3e57dbb96252c3a8a5190b2
Ebuka <peterdbrainy5@gmail.com>
2019-09-18 12:46:22 -0700

    A commit of all the edit task and add task modal, add task modal on the sub navigation icon has a bug

--- chatscrum/src/app/teamtaskcards/teamtaskcards.component.ts
+++ chatscrum/src/app/teamtaskcards/teamtaskcards.component.ts
@@ -12,48 +12,56 @@ export class TeamtaskcardsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    
-    // Initialise Modal  function
+    //Jquery here
+
+
+// Initialise Modal  function
    
-    window.addEventListener("load", modalControl)
-    function modalControl(){
-      let modalToggle = document.querySelectorAll(".modal_toggle");
-      let modal = document.querySelectorAll(".modal");
-      let overlay = document.querySelector(".overlay");
+window.addEventListener("load", modalControl)
+function modalControl(){
+  let modalToggle = document.querySelectorAll(".modal_toggle");
+  let modal = document.querySelectorAll(".modal");
+  let overlay = document.querySelector(".overlay");
 
-      modal.forEach( modal => {
-        let toggler = modal.parentElement;
+  modal.forEach( modal => {
+    let toggler = modal.parentElement;
 
-        // Add the click event to modal open icon to open when clicked
-        toggler.addEventListener("click", ()=>{
-          modalSwitch.show();
-        });
-          
+    // Add the click event to modal open icon to open when clicked
+    toggler.addEventListener("click", ()=>{
+      modalSwitch.show();
+    });
+      
 
-        // modal modes to control the opening and closing
-        var modalSwitch = {
-          show : ()=>{
-            overlay.classList.remove("hidden");
-            modal.classList.remove("hidden");
-          },
-          hide : ()=>{
-            overlay.classList.add("hidden");
-            modal.classList.add("hidden");
-          }
-        };
-
-        // Get the icon to close the modal 
-        let close = modal.querySelector("#cancel");
-
-        // Hide modal when user clicks outside the modal or on the close icon
-        window.addEventListener('click', function(event){
-          if (event.target === overlay || event.target === close ) {
-            modalSwitch.hide();
-          }
-        });
-      });
+    // modal modes to control the opening and closing
+    var modalSwitch = {
+      show : ()=>{
+        overlay.classList.remove("hidden");
+        modal.classList.remove("hidden");
+      },
+      hide : ()=>{
+        overlay.classList.add("hidden");
+        modal.classList.add("hidden");
+      }
     };
-    
-    
+
+    // Get the icon to close the modal 
+    let close = modal.querySelector("#cancel");
+
+    // Hide modal when user clicks outside the modal or on the close icon
+    window.addEventListener('click', function(event){
+      if (event.target === overlay || event.target === close ) {
+        modalSwitch.hide();
+      }
+    });
+  });
+};
+
+
+
+
   }
+
+
+  
+
 }
